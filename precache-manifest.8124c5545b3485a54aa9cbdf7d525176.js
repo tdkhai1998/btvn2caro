ff4c5b810181f226bf8a6a8494ef6d8cc50479f4
Dinh Khai Tran <trandinhkhai10.1@gmail.com>
2019-09-25 14:33:38 +0700

    Updates

---
+++ precache-manifest.8124c5545b3485a54aa9cbdf7d525176.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b76ea33ce3754f0e48104771e380872",
+    "url": "/btvn2caro/index.html"
+  },
+  {
+    "revision": "5dfb850b8d031b24544f",
+    "url": "/btvn2caro/static/css/main.335d7892.chunk.css"
+  },
+  {
+    "revision": "3ed9e73cd980c8d4ebf5",
+    "url": "/btvn2caro/static/js/2.4b72a531.chunk.js"
+  },
+  {
+    "revision": "5dfb850b8d031b24544f",
+    "url": "/btvn2caro/static/js/main.dcf7d763.chunk.js"
+  },
+  {
+    "revision": "5c44672059d591798c57",
+    "url": "/btvn2caro/static/js/runtime~main.e066835a.js"
+  }
+]);
