6bdd1005b204b1a195214c48e321ab390d33de81
Dinh Khai Tran <trandinhkhai10.1@gmail.com>
2019-09-27 16:06:34 +0700

    fix warnings errors for BusinessLogic

--- src/Components/BusinessLogic.js
+++ src/Components/BusinessLogic.js
@@ -1,43 +1,55 @@
-function haveWinner(square, i, width, height){
-    width=Number(width); height=Number(height);
-    const dir=[[[-1,-1], [1,1]],[[0,-1],[0,+1]],[[1,-1],[-1,1]],[[-1,0],[1,0]]];
-    for(var j=0;j<4;j++){
-        var result=countDirection(dir[j],i,square,width, height);
-        if(result!==false)// have a winner
-            return {
-                arr: result,
-                dir: j
-            }
+function countDirection(dir, i, square, width, height) {
+  let count = 0;
+  const turn = square[i].value;
+  let block = 0;
+  const mark = [];
+  const check = (x, y) => x >= 0 && x < width && (y >= 0 && y < height);
+  const countByTrend = trend => {
+    let x = i % width;
+    let y = Math.floor(i / width);
+    let index = i;
+    while (
+      check(x, y) &&
+      square[index] != null &&
+      square[index].value === turn
+    ) {
+      count += 1;
+      mark.push(index);
+      x += dir[trend][0];
+      y += dir[trend][1];
+      index = y * width + x;
     }
-    return false;
-}
-function countDirection(dir,i,square, width, height){
-  
-    var count=0, turn=square[i].value, block=0;
-    var mark=[];
-    var check=(x,y)=>(x>=0&&x<width) && (y>=0&&y<height);
-    var countByTrend=(trend)=>{
-        var x = i % width , y = Math.floor(i/width);
-        var index=i;
-        while(check(x,y) && square[index]!=null &&square[index].value===turn){
-            count++;
-            mark.push(index);
-            x+=dir[trend][0];
-            y+=dir[trend][1];
-            index=y*width+x;
-        }
-        if(check(x,y) && square[index]!=null && square[index].value!==turn){
-            block++;
-        }
+    if (check(x, y) && square[index] != null && square[index].value !== turn) {
+      block += 1;
     }
-    countByTrend(0);//up trend
-    countByTrend(1);//down trend
-    count--;
-    console.log(count);
-    if(count===5 && block===2) return false
-    if(count<5) return false;
-    return mark;
+  };
+  countByTrend(0); //  up trend
+  countByTrend(1); //  down trend
+  count -= 1;
+  if (count === 5 && block === 2) return false;
+  if (count < 5) return false;
+  return mark;
 }
-export {
-    haveWinner
-} 
+function haveWinner(square, i, width, height) {
+  const myWidth = Number(width);
+  const myHeight = Number(height);
+  const dir = [
+    [[-1, -1], [1, 1]],
+    [[0, -1], [0, +1]],
+    [[1, -1], [-1, 1]],
+    [[-1, 0], [1, 0]]
+  ];
+  for (let j = 0; j < 4; j += 1) {
+    const result = countDirection(dir[j], i, square, myWidth, myHeight);
+    if (result !== false)
+      // have a winner
+      return {
+        arr: result,
+        dir: j
+      };
+  }
+  return false;
+}
+export default {
+  haveWinner
+};
