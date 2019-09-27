6de7059b3e7c56d84826e7fd0e897e6be9f2a252
Dinh Khai Tran <trandinhkhai10.1@gmail.com>
2019-09-27 15:55:04 +0700

    fix warning error for Board.js

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,52 +1,114 @@
 import React from 'react';
-import Square from "./Square";
+import Square from './Square';
 
 class Board extends React.Component {
-    print() {
-        var row = []
-        let x = this.props.selectedIndex % 20;
-        let y = Math.floor(this.props.selectedIndex / 20);
-        for (var i = 0; i < this.props.n; i++) {
-            var squares = []
-            for (var j = 0; j < this.props.m; j++) {
-                const index = i * this.props.m + j;
-                if (i === y || j === x) {
-                    squares.push(<Square key={j} result={this.props.result} status='todam' value={this.props.square[index]} onClick={() => { this.props.onClick(index) }} />)
-                }
-                else {
-                    squares.push(<Square key={j} result={this.props.result} value={this.props.square[index]} onClick={() => { this.props.onClick(index) }} />)
-                }
-            }
-            row.push(<div key={i} className="board-row">{squares}</div>)
+  print() {
+    const { selectedIndex, n, m, result, square, onClick } = this.props;
+    const row = [];
+    const x = selectedIndex % 20;
+    const y = Math.floor(selectedIndex / 20);
+    for (let i = 0; i < n; i += 1) {
+      const squares = [];
+      for (let j = 0; j < m; j += 1) {
+        const index = i * m + j;
+        if (i === y || j === x) {
+          squares.push(
+            <Square
+              key={j}
+              result={result}
+              status="todam"
+              value={square[index]}
+              onClick={() => {
+                onClick(index);
+              }}
+            />
+          );
+        } else {
+          squares.push(
+            <Square
+              key={j}
+              result={result}
+              value={square[index]}
+              onClick={() => {
+                onClick(index);
+              }}
+            />
+          );
         }
-        return row
+      }
+      row.push(
+        <div key={i} className="board-row">
+          {squares}
+        </div>
+      );
     }
-    listTurn() {
-        var arr = !this.props.isSorted ? this.props.history.slice().reverse() : this.props.history.slice();
-        var indexHis = this.props.indexHistory;
-        return arr.map((item, index) => {
-            var postion = "( " + (item.index % 20) + " , " + Math.floor((item.index / 20)) + " ) ";
-            let index2 = !this.props.isSorted ? arr.length - 1 - index : index;
-            if (index2 === indexHis) {
-                return <button disabled className="myButton" key={index} onClick={() => this.props.amazing(index2)}>{index2} TURN {(item.turn ? 'X' : 'O') + ' ' + postion}</button>
-            }
-            return <button className="myButton" key={index} onClick={() => this.props.amazing(index2)}> {index2} TURN {(item.turn ? 'X' : 'O') + ' ' + postion}</button>
-        })
-    }
-    render() {
+    return row;
+  }
+
+  listTurn() {
+    const { isSorted, history, indexHistory, amazing } = this.props;
+    const arr = !isSorted ? history.slice().reverse() : history.slice();
+    const indexHis = indexHistory;
+    return arr.map((item, index) => {
+      const postion = `( ${item.index % 20} , ${Math.floor(
+        item.index / 20
+      )} ) `;
+      const index2 = !isSorted ? arr.length - 1 - index : index;
+      const id = index;
+      if (index2 === indexHis) {
         return (
-            <div >
-                <div className="flex-container">
-                    <div>{this.print()}</div>
-                    <div style={{ paddingTop: 10, width: 500 }}>
-                        <div style={{ width: 250 }}>{!this.props.result ? ("TURN " + (this.props.turn ? "X" : "O")) : ((this.props.turn ? "O" : "X") + " WON!!!")}</div>
-                        <button className="button" onClick={() => this.props.restart()}> RESTART</button>
-                        <button className="button" onClick={() => this.props.sort()}> {this.props.isSorted ? 'SORTED' : 'SORT'}</button>
-                        <div style={{ maxHeight: 450, overflow: 'auto', paddingBottom: 20 }}> {this.listTurn()}</div>
-                    </div>
-                </div>
+          <button
+            type="button"
+            disabled
+            className="myButton"
+            key={id}
+            onClick={() => amazing(index2)}
+          >
+            {index2} TURN {`${item.turn ? 'X' : 'O'} postion`}
+          </button>
+        );
+      }
+      return (
+        <button
+          type="button"
+          className="myButton"
+          key={id}
+          onClick={() => amazing(index2)}
+        >
+          {' '}
+          {index2} TURN {`${item.turn ? 'X' : 'O'}  ${postion}`}
+        </button>
+      );
+    });
+  }
+
+  render() {
+    const { result, turn, restart, sort, isSorted } = this.props;
+    return (
+      <div>
+        <div className="flex-container">
+          <div>{this.print()}</div>
+          <div style={{ paddingTop: 10, width: 500 }}>
+            <div style={{ width: 250 }}>
+              {!result
+                ? `TURN ${turn ? 'X' : 'O'}`
+                : `${turn ? 'O' : 'X'}  WON!!!`}
             </div>
-        )
-    }
+            <button type="button" className="button" onClick={() => restart()}>
+              RESTART
+            </button>
+            <button type="button" className="button" onClick={() => sort()}>
+              {isSorted ? 'SORTED' : 'SORT'}
+            </button>
+            <div
+              style={{ maxHeight: 450, overflow: 'auto', paddingBottom: 20 }}
+            >
+              {this.listTurn()}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Board;
