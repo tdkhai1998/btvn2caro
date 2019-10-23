466c75117b0f1dec133132affde5b75fa0946c0c
Dinh Khai Tran <trandinhkhai10.1@gmail.com>
2019-10-23 23:20:33 +0700

    Updates

--- precache-manifest.d92e0493725f6c3ca3b3128021260f54.js
+++ precache-manifest.d92e0493725f6c3ca3b3128021260f54.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6605d4b0b83d4758cf0e139cd08c5898",
+    "revision": "8639500b4601dac6727fa365f4453c09",
     "url": "/btvn2caro/index.html"
   },
   {
-    "revision": "ca5cdfb1be50813b8457",
+    "revision": "6aeb8b4e5f7f8103c5d4",
     "url": "/btvn2caro/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "4adaf710f06455d13504",
+    "revision": "61b83041fc86067fc695",
     "url": "/btvn2caro/static/css/main.8df70eb5.chunk.css"
   },
   {
-    "revision": "ca5cdfb1be50813b8457",
-    "url": "/btvn2caro/static/js/2.cdc21118.chunk.js"
+    "revision": "6aeb8b4e5f7f8103c5d4",
+    "url": "/btvn2caro/static/js/2.b3a3dea9.chunk.js"
   },
   {
-    "revision": "4adaf710f06455d13504",
-    "url": "/btvn2caro/static/js/main.3b2dc177.chunk.js"
+    "revision": "61b83041fc86067fc695",
+    "url": "/btvn2caro/static/js/main.a2028df1.chunk.js"
   },
   {
     "revision": "5c44672059d591798c57",
